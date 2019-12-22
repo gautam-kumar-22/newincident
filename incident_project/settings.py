5389de5aa028e25398619d058c0a7529047563a9
gautam <gautamcs22@gmail.com>
2019-12-22 20:11:37 +0530

    set allowed hosts

--- incident_project/settings.py
+++ incident_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!hk$d6=bn2j=9+$&6!etdrdlpv_-52f6h5x-y!#rol8bfvh^m+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 CRISPY_TEMPLATE_PACK = 'uni_form'
 # Application definition
